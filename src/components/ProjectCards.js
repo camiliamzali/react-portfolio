b1f5ecd3322882c3d6aebf1638ab4ae83b5c6ccb
Camilia <mzalicam@gmail.com>
2019-06-18 14:59:47 -0400

    created project cards

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -2,8 +2,13 @@ import React from './node_modules/react';
 
 function ProjectCards(props) {
   return (
-    <div classNameName="card">
-
+    <div class="card">
+      <img class="card-img-top" src={props.src} alt={props.alt} />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href={props.href} class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   )
 }
