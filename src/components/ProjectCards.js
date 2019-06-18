b2571227fddaf00b99a3635072d64f45730587a5
Camilia <mzalicam@gmail.com>
2019-06-18 15:00:13 -0400

    changed all class attributes to className

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -2,12 +2,12 @@ import React from './node_modules/react';
 
 function ProjectCards(props) {
   return (
-    <div class="card">
-      <img class="card-img-top" src={props.src} alt={props.alt} />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href={props.href} class="btn btn-primary">Go somewhere</a>
+    <div className="card">
+      <img className="card-img-top" src={props.src} alt={props.alt} />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href={props.href} className="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   )
